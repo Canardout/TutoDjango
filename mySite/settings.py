b87b921177c22de6cc3caffcdb77e3d7f235112a
Canardout <Canardout@gmail.com>
2018-03-16 16:09:34 +0100

    Finish : djangoGirl tutorial

--- mySite/settings.py
+++ mySite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8ml)bh5b8ruzufqnx33udnoi5ukf-cf!sl^*tzmj_lft)drvwq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['canardout.pythonanywhere.com']
+ALLOWED_HOSTS = ['canardout.pythonanywhere.com','127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutodjango',
+        'USER': 'jeremie',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -120,3 +124,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
